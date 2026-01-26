e8d2e555a386f7d2a2ccf94ef1adf9a916232967
Оля Набиева <52681127+Vacuoll@users.noreply.github.com>
2026-01-26 18:48:49 +0300

    upd

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "VIZHU-visuals-projects-map-0.1.05";
+const CACHE_NAME = "VIZHU-visuals-projects-map-0.1.06";
 // Файлы для предварительного кэширования
 const PRECACHE_URLS = [
   './',
